ef024a4694fe6951a187e104468ed33cf5b43a6a
Vladimir Kulikov <vladimir_kulikov@epam.com>
2020-11-10 12:21:52 +0300

    Get products list from RDS

---
+++ product-service/database/clientConfig.ts
@@ -0,0 +1,15 @@
+import { ClientConfig } from "pg";
+
+
+const { PG_HOST, PG_PORT, PG_DATABASE, PG_USERNAME, PG_PASSWORD } = process.env;
+export const CLIENT_CONFIG: ClientConfig = {
+    host: PG_HOST,
+    port: +PG_PORT,
+    database: PG_DATABASE,
+    user: PG_USERNAME,
+    password: PG_PASSWORD,
+    ssl: {
+        rejectUnauthorized: false
+    },
+    connectionTimeoutMillis: 5000
+}
--- product-service/handlers/getProductsList.ts
+++ product-service/handlers/getProductsList.ts
@@ -1,14 +1,33 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
+import { Client } from 'pg';
 import 'source-map-support/register';
-import productsList from '../database/productList.json';
+import { CLIENT_CONFIG } from "../database/clientConfig";
 
 
 export const getProductsList: APIGatewayProxyHandler = async () => {
-    return {
-        headers: {
-            'Access-Control-Allow-Origin': '*'
-        },
-        statusCode: 200,
-        body: JSON.stringify(productsList, null, 2),
-    };
+    const client = new Client(CLIENT_CONFIG);
+    const query = 'select * from products p left join stocks s on p.id = s.product_id';
+
+    try {
+        await client.connect();
+        const { rows: products } = await client.query(query);
+
+        return {
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            },
+            statusCode: 200,
+            body: JSON.stringify(products, null, 2),
+        };
+    } catch(e) {
+        return {
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            },
+            statusCode: 500,
+            body: 'Internal server error',
+        }
+    } finally {
+        await client.end();
+    }
 }
